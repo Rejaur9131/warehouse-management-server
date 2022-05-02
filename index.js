d33ce8736061e193caaa05a28b7483df16571b59
Rejaur9131 <raju_seu@yahoo.com>
2022-05-02 14:18:15 +0600

    worked with jwt

--- index.js
+++ index.js
@@ -3,21 +3,61 @@ const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // use middleware
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).send({ message: 'Unauthorized access' });
+  }
+  const token = authHeader?.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(403).send({ message: 'Access Forbidden' });
+    }
+    console.log('decoded', decoded);
+    req.decoded = decoded;
+  });
+
+  next();
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.syhej.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-const userCollection = client.db('wareHouse').collection('user');
 
 async function run() {
   try {
     await client.connect();
+    const userCollection = client.db('wareHouse').collection('user');
     console.log('Connected successfully to server');
+
+    // Auth
+    app.post('/login', verifyJWT, async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d'
+      });
+      res.send({ accessToken });
+    });
+
+    app.get('/user', async (req, res) => {
+      // const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+
+      // if (email === decodedEmail) {
+      const query = { email: email };
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+      // } else {
+      //   res.status(403).send({ message: 'Access Forbidden' });
+      // }
+    });
   } finally {
     // await client.close();
   }
@@ -29,15 +69,6 @@ app.get('/', (req, res) => {
   res.send('<h2>WareHouse Management Server</h2>');
 });
 
-app.get('/user', async (req, res) => {
-  const query = {};
-  const cursor = userCollection.find({});
-  const users = await cursor.toArray();
-  res.send(users);
-});
-
-// user:wareHouse
-// pass hxxF8OuwkMBJHC5C
 app.listen(port, () => {
   console.log('Listening to port:', port);
 });
